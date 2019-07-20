734018fc656030fb20e696e83bef1364e1badf1f
ODAO <anthony@osawere.com>
2019-07-20 12:09:44 +0100

    Create init.php

---
+++ project/config/init.php
@@ -0,0 +1,8 @@
+<?php
+define('oAPPMODE', 'dev'); #[DEV|BETA|PROD|OFF|MAINTENANCE] -R
+$oConfig = array();
+$oConfig['project'] = 'ZenQ';
+$oConfig['ver'] = '1.0';
+$oConfig['ifip'] = 'zern'; #path to app via IP
+$oConfig['url'] = 'zern.co'; #base URL to app
+?>
