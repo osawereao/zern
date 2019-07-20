cc30efe2f35fb483eee838a10fbe36ea02893f33
ODAO <anthony@osawere.com>
2019-07-20 12:12:55 +0100

    Create link.php

---
+++ project/config/link.php
@@ -0,0 +1,9 @@
+<?php
+//========== LINKS ALLOWED IN APPLICATION ==========//
+$oKonfig['link_allowed'] = array(
+	'default' => 'zern',
+	'index' => '',
+	'login' => 'auth', 'locked' => 'auth', 'logout' => 'auth',
+	'dashboard' => '',
+);
+?>
