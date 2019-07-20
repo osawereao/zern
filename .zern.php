a9391bd9b28d746a1241e56ed6fde85595d7cf7f
ODAO <anthony@osawere.com>
2019-07-20 12:41:28 +0100

    Update .zern.php

--- .zern.php
+++ .zern.php
@@ -3,79 +3,153 @@
  * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
  * © July 2019 | beta 1.0 | Apache License, Version 2.0
  * ===================================================================================================================
- * Dependency » index
- * PHP | zern::root ~ indispensable root functions
+ * Dependency » ~
+ * PHP | .zern::FW ~ the framework's main class
  **/
 
-/*Disable App when MODE is undefined or set to off*/
-if(!defined('oAPP_MODE') || oAPP_MODE == '' || oAPP_MODE == 'off'){exit;}
+class ZERN {
+	private $konfig;
 
-//========== SEPARATOR ==========//
-defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('PS') ? null : define('PS', '/');
+	//========== INITIALIZE ==========//
+	public function init()
+	{
+		/*** Disable App when MODE is undefined or set to off ***/
+		if(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'off'){exit;}
 
-
-//========== CONFIG ==========//
-function zernConfig($config='oCONFIG'){
-	if($config == 'oCONFIG'){
-		global $oConfig;
-		if(!empty($oConfig) && is_array($oConfig)){$config = $oConfig;}
+		/*** Separators ***/
+		defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+		defined('PS') ? null : define('PS', '/');
 	}
+	//****** END ******//
 
-	if(!empty($config) && is_array($config)){
-		return $config;
+
+	//========== GET CONFIGURATION ==========//
+	public function getKonfig($label='oKONFIG')
+	{
+		if(empty($label)){return 'ZE428-GK1: [Argument Required]';}
+		else {
+			$o = $this->konfig;
+			if(empty($o)){return 'ZE404-GK1: [No Data]';}
+			elseif(!is_array($o)){return 'ZE406-GK1: [Invalid Data]';}
+			else {
+				if($label == 'oKONFIG'){return $o;}
+				elseif(in_array($label, $o)){return $o[$label];}
+			}
+		}
+		return false;
 	}
-}
+	//****** END ******//
 
 
-//========== IP VALIDATOR [returns true/false] ==========//
-function zernValIP($host=''){
-	if(!empty($host)){
-		$parts = parse_url($host);
-		if(!isset($parts['host'])){$eval = (bool)ip2long($host);}
-		else {$eval = (bool)ip2long($parts['host']);}
-		return $eval;
+	//========== IP VALIDATOR [returns true/false] ==========//
+	public function isIP($host='')
+	{
+		if(empty($host)){return 'ZE428-IV1: [Argument Required]';}
+		else {
+			$parts = parse_url($host);
+			if(!isset($parts['host'])){$o = (bool)ip2long($host);}
+			else {$o = (bool)ip2long($parts['host']);}
+			return $o;
+		}
+		return false;
 	}
-	return false;
-}
+	//****** END ******//
 
 
-//========== ROOT (Directory & URL) ==========//
-function zernBP($task = 'oDIR', $path=''){
-	$eval = '';
-	if($task == 'oHOST'){
-		if(!empty($_SERVER["SERVER_NAME"])){
-			$eval = $_SERVER["SERVER_NAME"];
-			$config = zernConfig();
-			if((zernValIP($eval) || $eval == 'localhost') && !empty($config['ifip'])){
-				$eval = $eval.PS.$config['ifip'];
+	//========== BASEPATH [Root Directory & URL] ==========//
+	public function BP($task = 'oDIR', $path='oPREP')
+	{
+		if(empty($task)){exit('ZE428-BP1: [Argument Required]');}
+		elseif(empty($path)){exit('ZE428-BP2: [Argument Required]');}
+		elseif($task != 'oDIR' && $task != 'oHOST'){exit( 'ZE406-BP1: [Invalid Argument Input]');}
+		else {
+			$o = '';
+			if($path == 'oPREP'){
+				if($task == 'oHOST' && !empty($_SERVER["SERVER_NAME"])){
+					$o = $_SERVER["SERVER_NAME"];
+					$konfig = $this->konfig;
+					if(($this->isIP($o) || $o == 'localhost') && !empty($konfig['ifip'])){$o = $o.PS.$konfig['ifip'];}
+				}
+				elseif($task == 'oDIR' && !empty($_SERVER["DOCUMENT_ROOT"])){
+					$o = $_SERVER["DOCUMENT_ROOT"];
+				}
+			}
+			else {
+				if($task == 'oHOST'){
+					#TODO ~ check that $path is valid host
+					$o = $path;
+				}
+				elseif($task == 'oDIR'){
+					$pathinfo = pathinfo($path);
+					if(!empty($pathinfo['dirname'])){$o = $pathinfo['dirname'].DS;}
+				}
+			}
+			return $o;
+		}
+	}
+	//****** END ******//
+
+
+	//========== SET BASEPATH ==========//
+	public function setBP($task='', $path=''){
+		$o = $this->BP($task, $path);
+		if(!empty($o['oERROR'])){return $o;}
+		elseif(!empty($o) && $o !== false){
+			if($task == 'oHOST'){$this->oHost = $o;}
+			elseif($task == 'oDIR'){$this->oDir = $o;}
+			return true;
+		}
+		return false;
+	}
+	//****** END ******//
+
+
+	//========== DEBUG ==========//
+	public static function dbug($data, $printAs='', $continue='oYEAP'){
+		if($printAs == 'oJSON'){jsonResp($data);}
+		else {
+			echo '<p><em>Debugging</em></p><hr>';
+			if($printAs == 'oPRINT'){print_r($data);}
+			elseif($printAs == 'oDUMP'){var_dump($data);}
+			elseif($printAs == 'oEXPORT'){var_export($data);}
+			elseif($data === true){echo "Bool: TRUE";}
+			elseif($data === false){echo "Bool: FALSE";}
+			elseif(is_int($data)){echo $data;}
+			elseif(is_string($data) && $printAs == 'string'){echo $data;}
+			elseif(is_array($data)){
+				foreach ($data as $key => $value){
+					if(is_array($value)){
+						foreach ($value as $valueKey => $valueSub){
+							echo ' <strong>'.$key."</strong>['".$valueKey."']".': '.$valueSub.'<br>';
+						}
+					}
+					else {
+						echo '<strong>'.$key.':</strong> '. $value.'<br>';
+					}
+				}
+			}
+			else {
+				var_dump($data);
+			}
+		}
+		if($continue == 'oNOPE'){exit;}
+	}
+	//****** END ******//
+
+
+	//========== FILE LOADER [only add files] ==========//
+	public static function inc($file='', $eval='oREQUIRED'){
+		if(!empty($file) && !is_file($file)){$file = $file.'.php';}
+		if(file_exists($file)){require $file;}
+		elseif($eval == 'oREQUIRED'){
+			if(!defined('oAPPMODE') || oAPPMODE != 'dev'){
+				exit('ZE404-IF: '.basename($file));
+			}
+			else {
+				exit('Missing Library: '.$file);
 			}
 		}
 	}
-	elseif($task == 'oDIR'){
-		if(!empty($path)){
-			$pathinfo = pathinfo($path);
-			$eval = $pathinfo['dirname'].DS;
-		}
-		elseif(!empty($_SERVER["DOCUMENT_ROOT"])){
-			$eval = $_SERVER["DOCUMENT_ROOT"].DS;
-		}
-	}
-	return $eval;
-}
-
-
-//========== FILE LOADER (only add files) ==========//
-function zernLoad($file='', $as='oREQUIRED'){
-	if(!empty($file) && !is_file($file)){$file = $file.'.php';}
-	if(file_exists($file)){require $file;}
-	elseif($as == 'oREQUIRED'){
-		if(!defined('oAPP_MODE') || oAPP_MODE != 'dev'){
-			exit('ZE404: '.basename($file));
-		}
-		else {
-			exit('Missing Library: '.$file);
-		}
-	}
+	//****** END ******//
 }
 ?>
