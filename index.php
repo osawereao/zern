edcd8b5f94a3fc4bd2489e5e431e5ae227aac338
ODAO <anthony@osawere.com>
2019-07-20 12:09:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,35 +7,39 @@
  * PHP | index::root ~ default file
  **/
 
-//========== PROJECT CONFIGURATION -R ==========//
-define('oAPP_MODE', 'dev'); #[DEV|BETA|PROD|OFF|MAINTENANCE] -R
-$oConfig = array();
-$oConfig['project'] = 'ZenQ';
-$oConfig['ver'] = '1.0';
-$oConfig['ifip'] = 'zern'; #path to app via IP
-$oConfig['url'] = 'zern.co'; #base URL to app
-
-
-//========== ZERN PATH & LIBRARY ==========//
-if(file_exists('.zern.php')){
-	require '.zern.php';
-	$zernDIR = zernBP('oDIR', __FILE__);
-	$zernURL = zernBP('oHOST');
-	define('zernDIR', $zernDIR);
-	define('zernURL', $zernURL);
-	$zertInit = zernDIR.'zern'.DS.'init.php';
-	if(file_exists($zertInit)){
-		require $zertInit;
-		if((!defined('oPROJECT') || oPROJECT == '') || !file_exists(oPROJECT.'ignite.php')){
-			exit('ZE404B: Missing Ignition');
-		}
-		require oPROJECT.'ignite.php';
-	}
-	elseif(!defined('oAPP_MODE') || oAPP_MODE == '' || oAPP_MODE == 'dev'){
-		exit("ZE404B: Zern Initializer [{$zertInit}]");
-	}
+//========== ZERN PATH & LIBRARY, CONFIG ==========//
+$initKF = 'project/config/init.php';
+if(!file_exists($initKF)){
+	exit('ZE404A: CONFIG REQUIRED');
 }
-elseif(!defined('oAPP_MODE') || oAPP_MODE == '' || oAPP_MODE == 'dev'){
-	exit('ZE404A: Zern Root [.zern.php]');
+else {
+	require $initKF;
+	if(file_exists('.zern.php')){
+		require '.zern.php';
+		$zern = new ZERN;
+		$zern->init();
+		$zern->setBP('oDIR', __FILE__);
+		$zern->setBP('oHOST', 'oPREP');
+		define('zernDIR', $zern->oDir);
+		define('zernHost', $zern->oHost);
+		$initZF = zernDIR.'zern'.DS.'init.php';
+		if(!file_exists($initZF)){
+			if(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'dev'){
+				exit("ZE404B: ZERN INIT [{$initZF}]");
+			} else {
+				exit("ZE404B");
+			}
+		}
+		else {
+			require $initZF;
+			if((!defined('oPROJECT') || oPROJECT == '') || !file_exists(oPROJECT.'ignite.php')){
+				exit('ZE404C: Missing Ignition');
+			}
+			require oPROJECT.'ignite.php';
+		}
+	}
+	elseif(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'dev'){
+		exit('ZE404B: ZERN REQUIRED [.zern.php]');
+	}
 }
 ?>
