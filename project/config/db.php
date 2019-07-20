19fa86c361e0afcaafe0c8eaf219195535790bb5
ODAO <anthony@osawere.com>
2019-07-20 12:12:50 +0100

    Create db.php

---
+++ project/config/db.php
@@ -0,0 +1,11 @@
+<?php
+//========== DATABASE CONFIGURATION ==========//
+$oKonfig['db'] = array(
+	'name' => 'zenq',
+	'user' => 'zenq',
+	'pass' => 'ZenQ',
+	'host' => 'localhost',
+	'driver' => 'PDO',
+	'userz' => 'userz'
+);
+?>
