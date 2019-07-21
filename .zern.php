7a906a1aa97e2557d04d7eba91548b4319e0b654
ODAO <anthony@osawere.com>
2019-07-21 10:56:06 +0100

    Update .zern.php

--- .zern.php
+++ .zern.php
@@ -10,35 +10,26 @@
 class ZERN {
 	private $konfig;
 
+
 	//========== INITIALIZE ==========//
-	public function init()
+	public function init($zone='o9JA')
 	{
 		/*** Disable App when MODE is undefined or set to off ***/
-		if(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'off'){exit;}
+		if(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'OFF'){exit;}
 
 		/*** Separators ***/
 		defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 		defined('PS') ? null : define('PS', '/');
-	}
-	//****** END ******//
 
+		mb_internal_encoding("UTF-8");
+		ini_set('session.cache_limiter', 'public');
+		session_cache_limiter(false);
 
-	//========== GET CONFIGURATION ==========//
-	public function getKonfig($label='oKONFIG')
-	{
-		if(empty($label)){return 'ZE428-GK1: [Argument Required]';}
-		else {
-			$o = $this->konfig;
-			if(empty($o)){return 'ZE404-GK1: [No Data]';}
-			elseif(!is_array($o)){return 'ZE406-GK1: [Invalid Data]';}
-			else {
-				if($label == 'oKONFIG'){return $o;}
-				elseif(in_array($label, $o)){return $o[$label];}
-			}
-		}
-		return false;
+		#TODO ~ validate timezone's input
+		if($zone == 'o9JA'){$zone = 'Africa/Lagos';}
+		date_default_timezone_set($zone);
 	}
-	//****** END ******//
+	//==========** END **==========//
 
 
 	//========== IP VALIDATOR [returns true/false] ==========//
@@ -53,10 +44,10 @@ class ZERN {
 		}
 		return false;
 	}
-	//****** END ******//
+	//==========** END **==========//
 
 
-	//========== BASEPATH [Root Directory & URL] ==========//
+	//========== BASEPATH [root directory & url] ==========//
 	public function BP($task = 'oDIR', $path='oPREP')
 	{
 		if(empty($task)){exit('ZE428-BP1: [Argument Required]');}
@@ -67,8 +58,10 @@ class ZERN {
 			if($path == 'oPREP'){
 				if($task == 'oHOST' && !empty($_SERVER["SERVER_NAME"])){
 					$o = $_SERVER["SERVER_NAME"];
-					$konfig = $this->konfig;
-					if(($this->isIP($o) || $o == 'localhost') && !empty($konfig['ifip'])){$o = $o.PS.$konfig['ifip'];}
+					if(!empty($this->konfig)){
+						$konfig = $this->konfig;
+						if(($this->isIP($o) || $o == 'localhost') && !empty($konfig['ifip'])){$o = $o.PS.$konfig['ifip'];}
+					}
 				}
 				elseif($task == 'oDIR' && !empty($_SERVER["DOCUMENT_ROOT"])){
 					$o = $_SERVER["DOCUMENT_ROOT"];
@@ -87,11 +80,12 @@ class ZERN {
 			return $o;
 		}
 	}
-	//****** END ******//
+	//==========** END **==========//
 
 
 	//========== SET BASEPATH ==========//
-	public function setBP($task='', $path=''){
+	public function setBP($task='', $path='')
+	{
 		$o = $this->BP($task, $path);
 		if(!empty($o['oERROR'])){return $o;}
 		elseif(!empty($o) && $o !== false){
@@ -101,7 +95,143 @@ class ZERN {
 		}
 		return false;
 	}
-	//****** END ******//
+	//==========** END **==========//
+
+
+	//========== INITIALIZE APP & SET PROPERTIES ==========//
+	public function initApp($konfig='oKONFIG')
+	{
+		/*** Maintain PHP Session ***/
+		if(class_exists('oSession')){oSession::start();}
+
+		/*** Set Konfig Variable ***/
+		if(empty($konfig) || $konfig == 'oKONFIG'){
+			global $oKonfig;
+			if(!empty($oKonfig)){$konfig = $oKonfig;}
+		}
+
+		if(empty($konfig) || !is_array($konfig)){exit('Configuration is required');}
+		$this->konfig = $konfig;
+
+		/*** Continue Setting Variables ***/
+		foreach($konfig as $label => $value){
+			if(is_array($value) && $label != 'link_allowed'){
+				foreach($value as $sub_label => $sub_value){
+					$subLabel = $label . '_' . $sub_label;
+					$this->$subLabel = $sub_value;
+				}
+			}
+			else {
+				$this->$label = $value;
+			}
+		}
+
+		$this->setRoute();
+		$this->setURL();
+		if(isset($this->db_name)){$this->setDB();}
+	}
+	//==========** END **==========//
+
+
+	//========== GET CONFIGURATION ==========//
+	public function getKonfig($label='oKONFIG')
+	{
+		if(empty($label)){return 'ZE428-GK1: [Argument Required]';}
+		else {
+			$o = $this->konfig;
+			if(empty($o)){return 'ZE404-GK1: [No Data]';}
+			elseif(!is_array($o)){return 'ZE406-GK1: [Invalid Data]';}
+			else {
+				if($label == 'oKONFIG'){return $o;}
+				elseif(in_array($label, $o)){return $o[$label];}
+			}
+		}
+		return false;
+	}
+	//==========** END **==========//
+
+
+	//========== SET ROUTE ==========//
+	public function setRoute($eval=''){
+		if(class_exists('oURL')){
+			if(empty($this->route)){
+				$this->oRoute = oURL::route($eval);
+			}
+			$uriData = oURL::uriData();
+			if(!empty($uriData)){
+				$this->oURI = $uriData['uri'];
+				$this->oLink = $uriData['link'];
+				$this->oAction = $uriData['action'];
+				$this->oCase = $uriData['case'];
+			}
+		}
+		return;
+	}
+	//==========** END **==========//
+
+
+
+	//========== SET BASEURL ==========//
+	public function setURL(){
+		if(!empty($this->oHost)){$o = $this->oHost;}
+		elseif(defined('zernURL')){$o = zernURL;}
+		elseif(!empty($_SERVER["SERVER_NAME"])){$o = $_SERVER["SERVER_NAME"];}
+
+		if(!empty($o)){
+			if(oKit::hasSSL()){$o = 'https://'.$o;} else {$o = 'http://'.$o;}
+			$this->oURL = $o;
+			return true;
+		}
+		return false;
+	}
+	//==========** END **==========//
+
+
+	//========== CONFIGURE DATABASE & MAKE CONNCTION ==========//
+	public function setDB()
+	{
+		$db = array();
+		if(!empty($this->db_name)){$db['name'] = $this->db_name; unset($this->db_name);} else {$db['name'] = 'zenq';}
+		if(!empty($this->db_user)){$db['user'] = $this->db_user; unset($this->db_user);} else {$db['user'] = 'zenq';}
+		if(!empty($this->db_pass)){$db['pass'] = $this->db_pass; unset($this->db_pass);} else {$db['pass'] = 'ZenQ';}
+		if(!empty($this->db_host)){$db['host'] = $this->db_host; unset($this->db_host);} else {$db['host'] = 'localhost';}
+		if(!empty($this->db_table)){$db['table'] = $this->db_table; unset($this->db_table);} else {$db['table'] = 'userz';}
+		if(!empty($this->db_driver)){$db['driver'] = $this->db_driver; unset($this->db_driver);} else {$db['driver'] = 'PDO';}
+		if(!empty($db)){
+			#TODO ~ determine driver from config
+			if(class_exists('oPDO')){
+				$zernDB = new oPDO($db);
+				if(is_object($zernDB)){
+					$this->DB = $zernDB;
+
+					/*** Call Auth ***/
+					if(class_exists('oAuth')){
+						if(!empty($this->oURL)){$zernAuth = new oAuth($this->DB, $this->oURL);}
+						else {$zernAuth = new oAuth($this->DB);}
+						if(is_object($zernAuth)){$this->Auth = $zernAuth;}
+					}
+				}
+			}
+		}
+		return;
+	}
+	//==========** END **==========//
+
+
+	//========== FILE LOADER [only add files] ==========//
+	public static function inc($file='', $eval='oREQUIRED'){
+		if(!empty($file) && !is_file($file)){$file = $file.'.php';}
+		if(file_exists($file)){require $file;}
+		elseif($eval == 'oREQUIRED'){
+			if(!defined('oAPPMODE') || oAPPMODE != 'dev'){
+				exit('ZE404-IF: '.basename($file));
+			}
+			else {
+				exit('Missing Library: '.$file);
+			}
+		}
+	}
+	//==========** END **==========//
 
 
 	//========== DEBUG ==========//
@@ -134,22 +264,6 @@ class ZERN {
 		}
 		if($continue == 'oNOPE'){exit;}
 	}
-	//****** END ******//
-
-
-	//========== FILE LOADER [only add files] ==========//
-	public static function inc($file='', $eval='oREQUIRED'){
-		if(!empty($file) && !is_file($file)){$file = $file.'.php';}
-		if(file_exists($file)){require $file;}
-		elseif($eval == 'oREQUIRED'){
-			if(!defined('oAPPMODE') || oAPPMODE != 'dev'){
-				exit('ZE404-IF: '.basename($file));
-			}
-			else {
-				exit('Missing Library: '.$file);
-			}
-		}
-	}
-	//****** END ******//
+	//==========** END **==========//
 }
 ?>
