cd924f29cf0fd4bde083ec6aef00c0a5388091d9
ODAO <anthony@osawere.com>
2019-07-30 17:25:32 +0100

    Update db.php

--- project/config/db.php
+++ project/config/db.php
@@ -1,5 +1,12 @@
 <?php
-//========== DATABASE CONFIGURATION ==========//
+/**ZERN™ Framework ~ an evolving, robust platform for rapid & efficient development of modem responsive applications and APIs;
+ * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
+ * © July 2019 | beta 1.0 | Apache License, Version 2.0
+ * ===================================================================================================================
+ * Dependency » *
+ * PHP | db::konfig ~ database configuration
+ **/
+
 $oKonfig['db'] = array(
 	'name' => 'zern',
 	// 'user' => 'zenq',
