3ecc49daa5b42eb05306093560922e4dbf7f75bc
ODAO <anthony@osawere.com>
2019-07-30 17:27:35 +0100

    Update error.php

---
+++ project/config/error.php
@@ -0,0 +1,15 @@
+<?php
+/**ZERN™ Framework ~ an evolving, robust platform for rapid & efficient development of modem responsive applications and APIs;
+ * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
+ * © July 2019 | beta 1.0 | Apache License, Version 2.0
+ * ===================================================================================================================
+ * Dependency » *
+ * PHP | error::konfig ~ error messages & codes
+ **/
+
+$oErrorConfig = array();
+
+/*** LOGIN ***/
+
+
+?>
