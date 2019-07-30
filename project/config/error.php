728189d058411e90293ff7dc559466854e45e584
ODAO <anthony@osawere.com>
2019-07-30 17:50:14 +0100

    Update error.php

--- project/config/error.php
+++ project/config/error.php
@@ -13,9 +13,21 @@ $oErrorConfig = array();
 $oErrorConfig['login'] = array(
 	'E100A1' => 'Please enter your login details',
 
+	'E200A1' => 'Login successful',
+	'E200A2' => 'You have logged out successfully',
+
 	'E400A1' => 'Your UserID and password are required',
 	'E400A2' => 'Your UserID is required',
 	'E400A3' => 'Your password is required',
+
+	'E401A1' => 'Your password is incorrect',
+
+	'E404A1' => 'User account was not found!', #[oNORECORD]
+
+	'E501A1' => 'A program error occurred',
+
+	'E600B2' => 'Login failed', #query[oERROR]
+	'E600B3' => 'Operation failed', #query[false]
 );
 
 ?>
