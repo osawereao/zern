ce0c79a1dd9da65819b4a6ba830a2cc7a22228f3
ODAO <anthony@osawere.com>
2019-07-30 17:36:21 +0100

    Create auth.php

---
+++ project/suite/utilizr/auth.php
@@ -0,0 +1,15 @@
+<?php
+/**ZERN™ Framework ~ an evolving, robust platform for rapid & efficient development of modem responsive applications and APIs;
+ * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
+ * © July 2019 | beta 1.0 | Apache License, Version 2.0
+ * ===================================================================================================================
+ * Dependency » *
+ * PHP | auth::utilizer ~
+ **/
+
+// if(!empty($zernAuth)){
+// 	$active_user_data = $zernAuth->userActive();
+// 	$activeUser = new oData();
+// 	$activeUser->set($active_user_data);
+// }
+?>
