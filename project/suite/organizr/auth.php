fd6a24ec615dadda711320e129cb5c36028ab2a9
ODAO <anthony@osawere.com>
2019-08-03 18:38:40 +0100

    Update auth.php

--- project/suite/organizr/auth.php
+++ project/suite/organizr/auth.php
@@ -1,4 +1,5 @@
 <?php
+
 /**ZERN™ Framework ~ an evolving, robust platform for rapid & efficient development of modem responsive applications and APIs;
  * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
  * © July 2019 | beta 1.0 | Apache License, Version 2.0
@@ -11,44 +12,48 @@ $auth = array();
 
 
 //========== LOGIN ==========//
-if($zern->oLink == 'login'){
+if ($zern->oLink == 'login') {
 	$oprocez = 'oNOPE';
 
-	if($zernRoute == 'api'){
+	if ($zernRoute == 'api') {
 		$omethod = 'oGET'; #$omethod = 'oREQUEST';
 		$oprocez = 'oYEAP';
-	}
-	elseif($zernRoute == 'app'){
+	} elseif ($zernRoute == 'app') {
 		$omethod = 'oPOST';
-		if(empty($_POST)){
+		if (empty($_POST)) {
 			oSession::restart();
-			if(empty($_GET['zern'])){$auth ['oCODE'] = 'E100A1';}
-			else {
-				if($_GET['zern'] == 'logout'){$auth['oCODE'] = 'E200A2';}
+			if (empty($_GET['zern'])) {
+				$auth['oCODE'] = 'E100A1';
+			} else {
+				if ($_GET['zern'] == 'logout') {
+					$auth['oCODE'] = 'E200A2';
+				}
 			}
-		}
-		else {
+		} else {
 			$oprocez = 'oYEAP';
 		}
 	}
 
 
 	/*** PROCESS Login ***/
-	if($oprocez == 'oYEAP'){
+	if ($oprocez == 'oYEAP') {
 		$fita = array('userid', 'password');
 		$input = oInput::prep($omethod, $fita);
-		if(empty($input)){$auth['oCODE'] = 'E400A1';}
-		elseif(empty($input['userid'])){$auth['oCODE'] = 'E400A2';}
-		elseif(empty($input['password'])){$auth['oCODE'] = 'E400A3';}
-		else {
+		if (empty($input)) {
+			$auth['oCODE'] = 'E400A1';
+		} elseif (empty($input['userid'])) {
+			$auth['oCODE'] = 'E400A2';
+		} elseif (empty($input['password'])) {
+			$auth['oCODE'] = 'E400A3';
+		} else {
 			$input['userid'] = strtolower($input['userid']);
 			$auth = $zern->Auth->login($input['userid'], $input['password']);
-			if($auth['oCODE'] == 'E200A1' && $zernRoute == 'app'){
-				oURL::redirect($zern->oURL.PS.'dashboard');
+			if ($auth['oCODE'] == 'E200A1' && $zernRoute == 'app') {
+				oURL::redirect($zern->oURL . PS . 'dashboard');
 			}
 		}
-	} /*** PROCESS Login ~End ***/
-
+	}
+	/*** PROCESS Login ~End ***/
 }
 //========== LOGIN ~end ==========//
 
@@ -56,9 +61,11 @@ if($zern->oLink == 'login'){
 
 
 //========== UI LOADING [if module is not empty and router is app] ==========//
-if(!empty($auth)){
+if (!empty($auth)) {
 	$auth = oKit::response($auth, $zern->oLink);
-	if($zernRoute == 'app'){require oDESIGN.'auth.php';}
+	if ($zernRoute == 'app') {
+		require oDESIGN . 'auth.php';
+	}
 }
 
 
@@ -67,4 +74,3 @@ if(!empty($auth)){
 
 
 ZERN::dbug($auth);
-?>
