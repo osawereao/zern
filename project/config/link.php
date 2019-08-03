d1b337ef5a66221504087ab88bf3766a70e6a70b
ODAO <anthony@osawere.com>
2019-08-03 22:45:27 +0100

    Update link.php

--- project/config/link.php
+++ project/config/link.php
@@ -1,9 +1,9 @@
 <?php
 //========== LINKS ALLOWED IN APPLICATION ==========//
+$oKonfig['noauths'] = array('login', 'logout', 'locked', 'register');
 $oKonfig['link_allowed'] = array(
 	'default' => 'zern',
 	'index' => '',
 	'login' => 'auth', 'locked' => 'auth', 'logout' => 'auth',
-	'dashboard' => '',
+	'dashboard' => 'main',
 );
-?>
