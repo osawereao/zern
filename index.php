f5001dd0647af31eee7db5c8078e1b41a00ca89c
ODAO <anthony@osawere.com>
2019-08-03 22:41:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**ZERN™ Framework ~ an evolving, robust platform for rapid & efficient development of modem responsive applications and APIs;
  * Built by ODAO™ [www.osawere.com] using PHP, SQL, HTML, CSS, JS & derived technology.
  * © July 2019 | beta 1.0 | Apache License, Version 2.0
@@ -9,36 +10,32 @@
 
 //========== ZERN PATH & LIBRARY, CONFIG ==========//
 $initKF = 'project/config/init.php';
-if(!file_exists($initKF)){
+if (!file_exists($initKF)) {
 	exit('ZE404A: CONFIG REQUIRED');
-}
-else {
+} else {
 	require $initKF;
-	if(file_exists('.zern.php')){
+	if (file_exists('.zern.php')) {
 		require '.zern.php';
 		$zern = new ZERN($oKonfig);
-		$zern->init();
 		$zern->setBP('oDIR', __FILE__);
 		$zern->setBP('oHOST', 'oPREP');
 		define('zernDIR', $zern->oDir);
 		define('zernHOST', $zern->oHost);
-		$initZF = $zern->oDir.'zern'.DS.'init.php';
-		if(!file_exists($initZF)){
-			if(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'DEV'){
+		$initZF = $zern->oDir . 'zern' . DS . 'init.php';
+		if (!file_exists($initZF)) {
+			if (!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'DEV') {
 				exit("ZE404B: ZERN INIT [{$initZF}]");
 			} else {
 				exit("ZE404B");
 			}
-		}
-		else {
+		} else {
 			require $initZF;
-			if((!defined('oPROJECT') || oPROJECT == '') || !file_exists(oPROJECT.'ignite.php')){
+			if ((!defined('oPROJECT') || oPROJECT == '') || !file_exists(oPROJECT . 'ignite.php')) {
 				exit('ZE404C: PROJECT IGNITION');
 			}
-			require oPROJECT.'ignite.php';
+			require oPROJECT . 'ignite.php';
 		}
-	}
-	elseif(!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'DEV'){
+	} elseif (!defined('oAPPMODE') || oAPPMODE == '' || oAPPMODE == 'DEV') {
 		exit('ZE404B: ZERN REQUIRED [.zern.php]');
 	}
 }
